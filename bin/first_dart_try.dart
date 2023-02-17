18c6fd8afa9c8cd9c310be73255ca836caaac89f
vkany <vkrany@yandex.ru>
2023-02-17 13:43:21 +0300

    init commit

---
+++ bin/first_dart_try.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  List<String> names = ["Vova", "Lesha", "Kate"];
+  for (var person in names) {
+    print(person);
+  }
+  print("Hello World");
+}
---
+++ test/first_dart_try_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_dart_try/first_dart_try.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
